a1e35164d7b84b484fb811207f89b688289a0e11
hgaiji <yuukihigaki@MacBook-Pro.local>
2024-06-23 15:16:15 +0900

    portを動的に変更できるようにした

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net"
 	"os"
 	"log"
 	"context"
@@ -11,15 +12,23 @@ import (
 )
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+	if err := run(context.Background(),l); err != nil {
 		log.Printf("failed to terminated server: %v", err)
 		os.Exit(1)
 	}	
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "hello, %s!", r.URL.Path[1:])
 		}),
@@ -29,7 +38,7 @@ func run(ctx context.Context) error {
 	//
 
 	eg.Go(func()  error {
-		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err 
 		}
